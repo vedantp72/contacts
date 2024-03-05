26cdb5f042d8f2be2e85b0a6b7dd721195da4b06
vedantp72 <vedantpatel72@gmail.com>
2024-03-05 13:01:27 +0530

    changes

--- src/Components/ContactListing.js
+++ src/Components/ContactListing.js
@@ -18,9 +18,9 @@ function ContactListing() {
             });
     }, []);
     return (
-        <div className="container">
-            <div className="card">
-                <div className="card-title container text-center">
+        <div className="container-fluid">
+            <div className="container">
+                <div className="card-title text-center">
                     <h3>Contact Listing</h3>
                 </div>
                 <div className="card-body">
@@ -36,11 +36,11 @@ function ContactListing() {
                     <table className="table table-bordered table-hover">
                         <thead className="bg-dark text-white">
                             <tr className="text-center text-danger">
-                                <td>Name</td>
-                                <td>Email</td>
-                                <td>Phone</td>
-                                <td>Message</td>
-                                {/* <td>Action</td> */}
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Message</th>
+                                
                             </tr>
                         </thead>
                         <tbody className="App">
@@ -51,26 +51,7 @@ function ContactListing() {
                                         <td>{item.Email}</td>
                                         <td>{item.Phone}</td>
                                         <td>{item.Message}</td>
-                                        {/* <td>
-                                            <a
-                                                onClick={() => {
-                                                    LoadDetails(item.id);
-                                                }}
-                                                className="btn btn-sm btn-primary bi bi-eye me-2"
-                                            ></a>
-                                            <a
-                                                onClick={() => {
-                                                    EditDetails(item.id);
-                                                }}
-                                                className="btn btn-sm btn-warning bi bi-pen me-2"
-                                            ></a>
-                                            <a
-                                                onClick={() => {
-                                                    DeleteDetails(item.id);
-                                                }}
-                                                className="btn btn-sm btn-danger bi bi-trash "
-                                            ></a>
-                                        </td> */}
+                                       
                                     </tr>
                                 ))}
                         </tbody>
